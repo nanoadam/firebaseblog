d5cd7d8bb0bb4faa08157a4fed53d3791f1185e0
Ammar Razzak <razzakammaredu@gmail.com>
2019-09-21 17:13:11 -0400

    Initial Commit

---
+++ prepare/app.js
@@ -0,0 +1,13 @@
+// Firebase Config
+var firebaseConfig = {
+  apiKey: "AIzaSyAQxpjb7bGb7Omn35shKSOIqcyX-2RvmlE",
+  authDomain: "fir-blog-902f2.firebaseapp.com",
+  databaseURL: "https://fir-blog-902f2.firebaseio.com",
+  projectId: "fir-blog-902f2",
+  storageBucket: "",
+  messagingSenderId: "1060721683807",
+  appId: "1:1060721683807:web:7b03a4270de8004efbdaea"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
