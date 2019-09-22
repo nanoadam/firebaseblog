a6deaef2fcf24497ca2fb3796aacce33bbd4fb29
Nano Productions <razzakammar.edu@gmail.com>
2019-09-22 17:19:55 -0400

    Added

--- prepare/create.js
+++ prepare/create.js
@@ -1,17 +1,24 @@
-document.querySelector("#submitBtn").addEventListener("click", createPost);
-
-// Create Posts
-function createPost() {
+document.querySelector("#submitBtn").addEventListener("click", function() {
   let postAuthor = document.querySelector("#author").value;
   let postTitle = document.querySelector("#postTitle").value;
   let postContent = document.querySelector("#postContent").value;
   let postDate = document.querySelector("#postDate").value;
-  db.collection("posts")
-    .doc()
-    .set({
-      author: postAuthor,
-      createdAt: postDate,
-      postName: postTitle,
-      postContent: postContent
-    });
-}
+
+  if (
+    postAuthor === "" ||
+    postTitle === "" ||
+    postContent === "" ||
+    postDate === ""
+  ) {
+    alert("Fields Empty");
+  } else {
+    db.collection("posts")
+      .doc()
+      .set({
+        author: postAuthor,
+        createdAt: postDate,
+        postName: postTitle,
+        postContent: postContent
+      });
+  }
+});
